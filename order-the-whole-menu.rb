7a2f0ed6d89deee8774e0a4e13236a20e3ce43e4
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-08-04 13:17:32 -0500

    calculation a sum of all hash value

---
+++ order-the-whole-menu.rb
@@ -0,0 +1,17 @@
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+def total(order)
+  total = 0
+  order.each do |item, price|
+    total += price
+  end
+  return total
+end
+
+p total({ "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 })
