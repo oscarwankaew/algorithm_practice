33bbed60730c01d983a2e6c7f2a2493256eba94b
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-08-09 12:56:48 -0500

    solving etl problem using hash table and while loop

---
+++ etl.rb
@@ -0,0 +1,28 @@
+# You are given two parameters, an array and a number. Return a hash whose keys are each of the values from the array parameter, and whose values are the number parameter.
+
+# Input:
+
+# First argument: ["a", "e", "i", "o", "u"]
+# Second argument: 1
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'e' => 1,
+# 'i' => 1,
+# 'o' => 1,
+# 'u' => 1
+# }
+
+def hash(array, number)
+  index = 0
+  output = {}
+  while index < array.length
+    output[array[index]] = number
+    index += 1
+  end
+  return output
+end
+
+p hash(["a", "e", "i", "o", "u"], 1)
